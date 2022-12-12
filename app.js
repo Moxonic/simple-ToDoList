b6553950988524003087c9cdeea408df0c1a8f78
Daniel Soto Delgado <d.sotodelgado@outlook.com>
2022-12-12 14:45:31 +0100

    added templates and a date module

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-let ejs = require('ejs');
+const date = require(__dirname+ '/date.js');
 
 const app = express();
-var itemsAdded=['first item'];
+var itemsAdded=[];
+var workItemsAdded=[];
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true})); //necesarry for post request 
 app.use(express.static(__dirname + "/public"));     //set  static folder for server to reach files 
 
 app.get('/', function (req, res) {
-
-    var today = new Date();
-    var options = { weekday: 'long', day: 'numeric', month:'long'}
-    var day = today.toLocaleDateString('eng-US',options);
-
-    res.render('list', { dateString: day, newItems: itemsAdded })  /* render list.ejs and pass variables*/
+    res.render('list', { listTitle: date(), newItems: itemsAdded, route: '/' })  /* render list.ejs and pass variables*/
 });
 
 app.post('/', function(req,res){
+    console.log(req.body);
     itemsAdded.push(req.body.newTask);
-    
     res.redirect('/');
+})
+
+app.get('/work', function(req,res){
+    res.render('list', {listTitle:'Working List', newItems: workItemsAdded, route: '/work' })
+})
+app.post('/work', function(req,res){
+    workItemsAdded.push(req.body.newTask);
+    res.redirect('/work');
+})
+
+app.get('/about', (req,res)=>{
+    res.render('about');
 })
 
 app.listen(3000, function () {
